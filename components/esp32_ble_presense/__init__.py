7149fe4c2fb7d77dba753bba3b13032cd7d6f3d6
bmyers <ben.d.myers@gmail.com>
2024-08-09 12:07:18 -0500

    Update __init__.py

--- components/esp32_ble_presense/__init__.py
+++ components/esp32_ble_presense/__init__.py
@@ -27,7 +27,8 @@ CONFIG_SCHEMA = cv.Schema({
 
 async def to_code(config):
     cg.add_library(
-        name="esp-nimble-component",
+        name="esp-nimble-cpp",
+        version="1.4.1",
     )
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
